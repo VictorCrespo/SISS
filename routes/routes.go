15fc98d2d696bd2c4b35c97bc76a19678245e073
VictorCrespo <VictorCrespo.Developer@gmail.com>
2022-11-08 20:20:18 -0800

    Se agrego la ruta para obtener todos los usuarios

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
+	"github.com/VictorCrespo/SISS/handlers"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
 func RegisterRoutes(r *mux.Router, db *gorm.DB) {
-
+	r.HandleFunc("/usuarios", handlers.GetUsuarios(db)).Methods("GET")
 }
