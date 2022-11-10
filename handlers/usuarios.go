93995ed1ff71d335ce6496ce401dde201306bccd
VictorCrespo <VictorCrespo.Developer@gmail.com>
2022-11-09 21:20:09 -0800

    Se agregaron metodos para usuario

--- handlers/usuarios.go
+++ handlers/usuarios.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/VictorCrespo/SISS/models"
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -27,5 +28,114 @@ func GetUsuarios(db *gorm.DB) http.HandlerFunc {
 			w.Write([]byte(err.Error()))
 			return
 		}
+	}
+}
+
+func GetUsuario(r *mux.Router, db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		var u models.Usuario
+		params := mux.Vars(r)
+
+		result := db.First(&u, params["id"])
+		if result.Error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(result.Error.Error()))
+			return
+		}
+
+		err := json.NewEncoder(w).Encode(u)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+	}
+}
+
+func CreateUsuario(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+
+		var u models.Usuario
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		result := db.Create(&u)
+		if result.Error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(result.Error.Error()))
+			return
+		}
+	}
+}
+
+func UpdateUsuario(r *mux.Router, db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+
+		var u models.Usuario
+		params := mux.Vars(r)
+
+		result := db.First(&u, params["id"])
+		if result.Error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(result.Error.Error()))
+			return
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		result = db.Save(&u)
+		if result.Error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(result.Error.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func DeleteUsuario(r *mux.Router, db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+
+		var u models.Usuario
+
+		params := mux.Vars(r)
+		result := db.First(&u, params["id"])
+		if result.Error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(result.Error.Error()))
+			return
+		}
+
+		if u.Usuario_id == 0 {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("No se encontro el id"))
+			return
+		}
+
+		result = db.Delete(&u)
+		if result.Error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(result.Error.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
