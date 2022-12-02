6fb6e07b573e743c2226a6365413e154c8848779
VictorCrespo <VictorCrespo.Developer@gmail.com>
2022-12-01 21:32:56 -0800

    Se agrego una funcion AuthJwtToken a la clase middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,35 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/VictorCrespo/SISS/authentication"
+	"github.com/VictorCrespo/SISS/models"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v4/request"
+)
+
+func AuthJwtToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		//w.WriteHeader(http.StatusOK)
+
+		token, err := request.ParseFromRequest(r, request.OAuth2Extractor, func(t *jwt.Token) (interface{}, error) {
+			return authentication.PublicKey, nil
+		}, request.WithClaims(&models.Claim{}))
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Your token is not valid"))
+			return
+		}
+
+		if token.Valid {
+			next.ServeHTTP(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Your token is not valid"))
+			return
+		}
+	})
+}
